564076c3e53c5d2ab809dd956653b662b2ed149b
Alexander Dahl <alex@netz39.de>
2018-12-18 11:24:52 +0100

    Revert "main: Circumvent out of bounds read/write"
    This reverts commit 9eebe6528ce7ab3f1b7df3b190315b116a2da0c8.
    
    After fixing the types in 1a122a2cb36dc6ac480118a2d3f0b8f3097ed879
    valgrind does not complain anymore about invalid read/write, even if we
    don't use the workaround with a bigger buffer than actually used.

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@ int main(int argc, const char **argv)
   int keybytes = static_cast<int>(keystr.size());
   bfc->Initialize(key, keybytes);
 
-  unsigned long bfBufSize = bfc->GetOutputLength(original.size()+9);
+  unsigned long bfBufSize = bfc->GetOutputLength(original.size()+1);
   unsigned char *bfBuf = new unsigned char[bfBufSize];
   std::fill(bfBuf, bfBuf + bfBufSize, 0);
   original.copy(reinterpret_cast<char *>(bfBuf), original.size());
 
-  bfc->Encode(bfBuf, bfBuf, bfBufSize-8);
+  bfc->Encode(bfBuf, bfBuf, bfBufSize);
 
-  bfc->Decode(bfBuf, bfBuf, bfBufSize-8);
+  bfc->Decode(bfBuf, bfBuf, bfBufSize);
 
   std::string decoded = std::string(reinterpret_cast<char *>(bfBuf));
 
